de833a5392f201e287646cacdf3edeea39c4e698
HassanELsherbini <h.elsherbini7@gmail.com>
2018-02-07 16:51:52 -0500

    search near me done

--- client/components/search-bar.js
+++ client/components/search-bar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form } from 'semantic-ui-react';
+import { Form, Icon, Button, Label } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import {fetchEventsByKeyword, fetchEventsByLocation} from '../store';
 
@@ -10,33 +10,38 @@ class SearchBar extends Component {
     super();
     this.state = {keyword: ''};
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(event) {
     this.setState({keyword: event.target.value})
   }
 
-  handleSubmit(event) {
+  handleClick(event) {
+    console.log('adasd')
     event.preventDefault();
-    if (this.state.keyword.length){
-      const keyword = this.state.keyword;
-      this.setState({ keyword: '' });
-      this.props.searchEvents(keyword);
-    }
+    // if (this.state.keyword.length){
+    //   const keyword = this.state.keyword;
+    //   this.setState({ keyword: '' });
+    //   this.props.searchEvents(keyword);
+    // }
+    this.props.searchEvents('10004');
   }
 
   render() {
     return (
-    <div>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Input
-          action={{ icon: 'search' }}
-          onChange={this.handleChange}
-          value={this.state.keyword}
-          placeholder="Search Events by ZIP Code..." />
-        </Form>
-    </div>
+
+      <Button  animated='fade' circular compact={true} onClick={this.handleClick}>
+        <Button.Content visible >
+          <Icon name='marker' size='big' color='blue'/>
+        </Button.Content>
+        <Button.Content hidden>
+          Near Me
+        </Button.Content >
+      </Button>
+
+
+
     )
   }
 }
@@ -54,3 +59,11 @@ const mapDispatch = (dispatch) => {
 export default connect(null, mapDispatch)(SearchBar);
 
 
+
+  /* /* <Form onSubmit={this.handleSubmit}>
+          <Form.Input
+          action={{ icon: 'search' }}
+          onChange={this.handleChange}
+          value={this.state.keyword}
+          placeholder="Search Events by ZIP Code..." />
+        </Form> */
